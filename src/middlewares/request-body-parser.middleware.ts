d4646b953e0bb7dd78562adf9c785458b3637e85
Amit Dhawan <amit.dhawan@outlook.com>
2020-02-05 10:34:51 +0530

    Removed raw-body npm package

--- src/middlewares/request-body-parser.middleware.ts
+++ src/middlewares/request-body-parser.middleware.ts
@@ -2,13 +2,18 @@ import {
     Injectable,
     NestMiddleware
 } from '@nestjs/common';
-import * as getRawBody from 'raw-body';
 
 @Injectable()
 export class RequestBodyParserMiddleware implements NestMiddleware {
     async use(req: any, res: any, next: any) {
         console.log('Im here');
-        const body = await getRawBody(req);
-        next();
+        let data = '';
+        req.on('data', (chunk) => {
+            data+=chunk;
+        })
+        req.on('end', () => {
+            console.log(data);
+            next();
+        })
     }
 }
