8d64c85c83fe485cf382deaafc0a3dc56530afd2
Amit Dhawan <amit.dhawan@outlook.com>
2020-02-05 10:42:32 +0530

    Test with getrawbody funtion

--- src/middlewares/request-body-parser.middleware.ts
+++ src/middlewares/request-body-parser.middleware.ts
@@ -2,18 +2,13 @@ import {
     Injectable,
     NestMiddleware
 } from '@nestjs/common';
+import * as getRawBody from 'raw-body';
 
 @Injectable()
 export class RequestBodyParserMiddleware implements NestMiddleware {
     async use(req: any, res: any, next: any) {
-        console.log('Im here');
-        let data = '';
-        req.on('data', (chunk) => {
-            data+=chunk;
-        })
-        req.on('end', () => {
-            console.log(data);
-            next();
-        })
+        const body = await getRawBody(req);
+        console.log('BODY', body.toString());
+        next();
     }
 }
